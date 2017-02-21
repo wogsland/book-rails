452630303f5d1137002f94eaee59fbff6610fbfa
Bradley Wogsland <bradley@wogsland.org>
2017-02-21 17:08:13 -0600

    I broke the tests...

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'book/update'
-
   get 'book/add'
   get 'book/list'
+  get 'welcome/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
